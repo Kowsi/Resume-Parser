adf542933bf63d089123b0c49886193be3b62921
Kowsalya Jeyabalan <kowsi.ssn@gmail.com>
2019-12-03 15:50:13 -0500

    Update ResumeParser.py

--- ResumeParser.py
+++ ResumeParser.py
@@ -4,37 +4,34 @@ import re
 from spacy.matcher import Matcher
 from spacy.tokens import Span
 
-
-PERSON_PATTERN = [{'POS': 'PROPN', 'ENT_TYPE': 'PERSON'},
-                  {'POS': 'PROPN', 'ENT_TYPE': 'PERSON', 'OP': '?'},
-                  {'POS': 'PROPN', 'ENT_TYPE': 'PERSON'}]
-
-EMAIL_ID_PATTERN = [{"LIKE_EMAIL": True}]
-
-PHONE_PATTERN_1 = [{"SHAPE": "ddd"}, {"ORTH": "-", "OP": "?"},
-                   {"SHAPE": "ddd"}, {"ORTH": "-", "OP": "?"},
-                   {"SHAPE": "dddd"}]
-
-PHONE_PATTERN_2 = [{"ORTH": "("}, {"SHAPE": "ddd"}, {"ORTH": ")"},
-                   {"SHAPE": "ddd"}, {"ORTH": "-", "OP": "?"},
-                   {"SHAPE": "dddd", "LENGTH": {"==": 4}}]
-
-ADDRESS_PATTERN = [{'POS': 'NUM'}, {'POS': 'PROPN'},
-                   {'POS': 'PROPN', 'OP': "*"},
-                   {'IS_PUNCT': True, 'OP': '?'},
-                   {'LEMMA': {'IN': ['apt', 'unit']}, 'OP': '?'},
-                   {'POS': 'NUM'},
-                   {'ORTH': ',', 'OP': "?"},
-                   {'POS': 'PROPN'},
-                   {'ORTH': {
-                     'REGEX': '(AK|AL|AR|AZ|CA|CO|CT|DC|DE|FL|GA|GU|HI|IA|ID|IL|IN|KS|KY|LA|MA|MD|ME|MI|MN|MO|MS|MT|NC|ND|NE|NH|NJ|NM|NV|NY|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VA|VI|VT|WA|WI|WV|WY)'}}]
-
-LINKEDIN_URL_PATTERN = [{"LIKE_URL": True, 'ORTH': {'REGEX': r'\s*(linkedin.com)\s*'}}]
-
-GIT_URL_PATTERN = [{"LIKE_URL": True, 'ORTH': {'REGEX': r'\s*(github.com)\s*'}}]
-
-
 class MatchEvent:
+    PERSON_PATTERN      = [{'POS': 'PROPN', 'ENT_TYPE': 'PERSON'},
+                          {'POS': 'PROPN', 'ENT_TYPE': 'PERSON', 'OP': '?'},
+                          {'POS': 'PROPN', 'ENT_TYPE': 'PERSON'}]
+
+    EMAIL_ID_PATTERN    = [{"LIKE_EMAIL": True}]
+
+    PHONE_PATTERN_1     = [{"SHAPE": "ddd"}, {"ORTH": "-", "OP": "?"},
+                           {"SHAPE": "ddd"}, {"ORTH": "-", "OP": "?"},
+                           {"SHAPE": "dddd"}]
+
+    PHONE_PATTERN_2     = [{"ORTH": "("}, {"SHAPE": "ddd"}, {"ORTH": ")"},
+                           {"SHAPE": "ddd"}, {"ORTH": "-", "OP": "?"},
+                           {"SHAPE": "dddd", "LENGTH": {"==": 4}}]
+
+    ADDRESS_PATTERN     = [{'POS': 'NUM'}, {'POS': 'PROPN'},
+                           {'POS': 'PROPN', 'OP': "*"},
+                           {'IS_PUNCT': True, 'OP': '?'},
+                           {'LEMMA': {'IN': ['apt', 'unit']}, 'OP': '?'},
+                           {'POS': 'NUM'},
+                           {'ORTH': ',', 'OP': "?"},
+                           {'POS': 'PROPN'},
+                           {'ORTH': {
+                               'REGEX': '(AK|AL|AR|AZ|CA|CO|CT|DC|DE|FL|GA|GU|HI|IA|ID|IL|IN|KS|KY|LA|MA|MD|ME|MI|MN|MO|MS|MT|NC|ND|NE|NH|NJ|NM|NV|NY|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VA|VI|VT|WA|WI|WV|WY)'}}]
+
+    LINKEDIN_URL_PATTERN = [{"LIKE_URL": True, 'ORTH': {'REGEX': r'\s*(linkedin.com)\s*'}}]
+
+    GIT_URL_PATTERN     = [{"LIKE_URL": True, 'ORTH': {'REGEX': r'\s*(github.com)\s*'}}]
 
     @classmethod
     def full_name_event(cls, matcher, doc, i, matches):
@@ -47,23 +44,23 @@ class MatchEvent:
 
 
 class ResumeParser:
-  
+
     nlp = spacy.load("en_core_web_sm")
 
-    CANDIDATE_INFO = [{'id': 'FullName',    'match_on': MatchEvent.full_name_event, 'pattern': PERSON_PATTERN},
-                      {'id': 'Email',       'match_on': None,                       'pattern': EMAIL_ID_PATTERN},
-                      {'id': 'Address',     'match_on': None,                       'pattern': ADDRESS_PATTERN},
-                      {'id': 'Phone',       'match_on': None,                       'pattern': PHONE_PATTERN_1},
-                      {'id': 'Phone',       'match_on': None,                       'pattern': PHONE_PATTERN_2},
-                      {'id': 'GithubURL',   'match_on': None,                       'pattern': GIT_URL_PATTERN},
-                      {'id': 'LinkedInURL', 'match_on': None,                       'pattern': LINKEDIN_URL_PATTERN}]
+    CANDIDATE_INFO = [{'id': 'FullName',    'match_on': MatchEvent.full_name_event, 'pattern': MatchEvent.PERSON_PATTERN},
+                      {'id': 'Email',       'match_on': None,                       'pattern': MatchEvent.EMAIL_ID_PATTERN},
+                      {'id': 'Address',     'match_on': None,                       'pattern': MatchEvent.ADDRESS_PATTERN},
+                      {'id': 'Phone',       'match_on': None,                       'pattern': MatchEvent.PHONE_PATTERN_1},
+                      {'id': 'Phone',       'match_on': None,                       'pattern': MatchEvent.PHONE_PATTERN_2},
+                      {'id': 'GithubURL',   'match_on': None,                       'pattern': MatchEvent.GIT_URL_PATTERN},
+                      {'id': 'LinkedInURL', 'match_on': None,                       'pattern': MatchEvent.LINKEDIN_URL_PATTERN}]
 
     def __init__(self, file_name):
         self.txt = self.convert_docx2txt(file_name)
         self.doc = ResumeParser.nlp(self.txt)
         self.matcher = Matcher(self.nlp.vocab, validate=True)
 
-        
+
     # Converting Docx to txt using docx2txt
     def convert_docx2txt(self, file_name):
         temp = docx2txt.process(file_name)
@@ -104,6 +101,4 @@ def main():
     rp = ResumeParser('./Sample Data/Manish_ARORA_Profile.docx')
     print(rp.get_candidate_info())
 
-
 main()
-
